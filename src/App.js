8857a4bf58bb8073c4b9fb1c769816f874ecf7d1
AleDecre <s291974@studenti.polito.it>
2024-03-18 09:50:08 +0100

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test CI/CD
+          Test CI/CD NUOVOOOO
         </p>
         <a
           className="App-link"

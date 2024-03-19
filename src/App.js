f707e0f258410fbadb94335fa3e4854fc6c4f718
Alessandro De Crecchio <61276160+AleDecre@users.noreply.github.com>
2024-03-19 10:33:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test CI/CD NUOVOOOO
+          Test CI/CD
         </p>
         <a
           className="App-link"

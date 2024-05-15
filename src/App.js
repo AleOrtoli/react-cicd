377976b2b3517d471ff59af9bfb72220d8aeb0f7
Alessandro Ortoli <136074411+AleOrtoli@users.noreply.github.com>
2024-05-15 12:34:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          Learn React ciao mondo!
         </a>
       </header>
     </div>
